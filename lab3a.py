63ebf73b9f3b7f7d3895db9c82a4284cecdd2e71
kirandeep-kaur51 <kirandeep-kaur51@myseneca.ca>
2024-06-03 11:56:32 -0400

    Add files via upload

---
+++ lab3a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+#return_text_value():
+#Author ID: kirandeep-kaur51@myseneca.ca
+
+def return_text_value():
+    return "Good Morning Terry" 
+
+#return_number_value() Function
+
+def return_number_value():
+    a = 10
+    b = 5
+    return a +b
+
+
+#Main Program
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
+
